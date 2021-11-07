2a5bf61a2ed01316e1fe9fc491a8310e1304e183
Isaac Mattern <isaac.mattern@gmail.com>
2021-11-07 01:34:28 -0400

    fix bug AGAIN

--- components/report.js
+++ components/report.js
@@ -1,15 +1,110 @@
+import React, { useState } from 'react'
 import MonthlyRx from './monthly-rx.js'
 import PopularRx from './popular-rx.js'
 import NewPopularRx from './newpopular-rx'
 import CountryDrugGrowthRates from './CountryDrugGrowthRates.js'
+import { forIn } from 'lodash'
 
 const Report = ({ data }) => {
+  const [drugFilter, setDrugFilter] = useState("All")
+  const [stateFilter, setStateFilter] = useState("All")
+
+  const generateFilters = () => {
+    return (<>
+      <div className="my-4 flex flex-col space-y-4">
+        <h4>Filter Results</h4>
+        <div className="flex flex-col">
+          {generateDrugFilters()}
+        </div>
+        <div className="flex flex-col">
+          {generateStateFilters()}
+        </div>
+      </div>
+    </>)
+  }
+
+  const generateDrugFilters = () => {
+    const uniqueDrugs = [...new Set(data.map(item => item.product))].sort()
+    uniqueDrugs = uniqueDrugs.filter(function (element) {
+      return element !== undefined;
+    })
+    const drugs = uniqueDrugs.map(drug => (
+      <label className={`flex flex-row items-center border border-gray-500 py-1 px-4 rounded-lg ${drugFilter === drug ? "bg-blue-500 text-white border-transparent" : null}`}>
+        <input className="appearance-none" type="radio" name="drugs" value={drug} checked={drugFilter === drug} onChange={event => { setDrugFilter(event.target.value) }} />
+        <span>{drug}</span>
+      </label>
+    ))
+
+    return (<>
+      <label htmlFor="drugs">Products:</label>
+      <div className="flex flex-row space-x-4">
+        <label className={`flex flex-row items-center border border-gray-500 py-1 px-4 rounded-lg ${drugFilter === "All" ? "bg-blue-500 text-white border-transparent" : null}`}>
+          <input className="appearance-none" type="radio" name="drugs" value="All" checked={drugFilter === "All"} onChange={event => setDrugFilter(event.target.value)} />
+          <span>All</span>
+        </label>
+        {drugs}
+      </div>
+    </>)
+  }
+
+  const generateStateFilters = () => {
+    const uniqueStates = [...new Set(data.map(item => item.state))].sort()
+    uniqueStates = uniqueStates.filter(function (element) {
+      return element !== undefined;
+    })
+    const states = uniqueStates.map(state => (
+      <option key={state} value={state}>{state}</option>
+    ))
+
+    return (<>
+      <label htmlFor="states">State</label>
+      <select className="border border-gray-500 py-1 px-4 rounded-lg w-min" name="states" value={stateFilter} onChange={event => setStateFilter(event.target.value)}>
+        <option value="All">All</option>
+        {states}
+      </select>
+    </>)
+  }
+
+  const filterIfNecessary = (data) => {
+    var dataCopy = JSON.parse(JSON.stringify(data));
+
+    let result = dataCopy
+    // console.log("result before: ", result)
+    if (drugFilter !== "All") {
+      result = result.filter(doctor => {
+        // console.log(drugFilter)
+        // console.log('product: ' + doctor.product)
+        return (doctor.product === drugFilter)
+      })
+      console.log("result after: ", result)
+    }
+    if (stateFilter !== "All") {
+      result = result.filter(doctor => doctor.state === stateFilter)
+    }
+    // console.log(result)
+    return result
+  }
+
+  const displayFirst = () => {
+    const filteredData = filterIfNecessary(dataCopy)
+    console.log(filteredData[0])
+    return (
+      <h4>The first doctor in the array is {filteredData[0].first_name} {filteredData[0].last_name}.</h4>
+    )
+  }
+
   return (
     <div className="w-full">
       <h1 className="text-3xl font-bold">Prescriber Data Report</h1>
       <h3 className="text-lg font-bold" >Generated at {/* TODO: Add time of generation. */}</h3>
       {/* <p>The first doctor's name is {data[0].first_name}.</p> */}
-      <div className="grid md:grid-cols-2 gap-8 mt-8 text-center">
+
+
+      {generateFilters()}
+
+      {/* {displayFirst()} */}
+
+      <div className="grid md:grid-cols-2 gap-8 mt-16 text-center">
         <div className="flex flex-col space-y-4">
           <p className="uppercase text-xs text-bold w-full">First doctor's monthly new prescriptions</p>
           <ShowNewMonthly data={data} />
@@ -31,53 +126,53 @@ const Report = ({ data }) => {
           <CreateNewMostPopularDrug data={data} />
         </div>
         <div className="flex flex-col space-y-4">
-        <p className="uppercase text-xs text-bold">Top-Selling Doctors</p>
-          <GetBestDoctor data={data} />
+          <p className="uppercase text-xs text-bold">Top-Selling Doctors</p>
+          <GetBestDoctor data={filterIfNecessary(data)} />
         </div>
       </div>
     </div>
   )
 }
 
-const GetBestDoctor = ( {data} ) => {
+const GetBestDoctor = ({ data }) => {
 
-  console.warn(data)
+  var dataCopy = JSON.parse(JSON.stringify(data));
 
+  if (dataCopy.length == 0) {
+    return <h3>No data available under current filters.</h3>
+  }
   const reducer = (previousValue, currentValue) => previousValue + currentValue
-  const sortedData = data.sort((a,b)=>{
+  const sortedData = dataCopy.sort((a, b) => {
     if (a.total_rx.reduce(reducer, 0) > b.total_rx.reduce(reducer, 0)) {
       return -1
     } else {
       return 1
-  }})
+    }
+  })
+
+  const tabledata = () => {
+    let result = []
+    for (let i = 0; i < Math.min(sortedData.length, 5); i += 1) {
+      result.push(
+        <tr>
+          <td className="font-bold px-4">{i + 1}</td>
+          <td className="text-left px-4">{sortedData[i].first_name + " " + sortedData[0].last_name}</td>
+          <td className="px-4">{sortedData[i].total_rx.reduce((a, b) => a + b, 0)}</td>
+        </tr>
+      )
+    }
+    return result
+  }
 
   return (
     <div className="flex justify-center">
-      <table className="table-auto border border-black">
+      <table className="table-auto border border-black p-4">
         <tr className="font-bold">
-          <th>Doctor</th>
-          <th>Total Drugs Sold</th>
+          <th className="px-4"></th>
+          <th className="px-4">Doctor</th>
+          <th className="px-4">Total Drugs Sold</th>
         </tr>
-        <tr>
-          <td>{sortedData[0].first_name + " " + sortedData[0].last_name}</td>
-          <td>{sortedData[0].total_rx.reduce((a, b) => a + b, 0)}</td>
-        </tr>
-        <tr>
-          <td>{sortedData[1].first_name + " " + sortedData[1].last_name}</td>
-          <td>{sortedData[1].total_rx.reduce((a, b) => a + b, 0)}</td>
-        </tr>
-        <tr>
-          <td>{sortedData[2].first_name + " " + sortedData[2].last_name}</td>
-          <td>{sortedData[2].total_rx.reduce((a, b) => a + b, 0)}</td>
-        </tr>
-        <tr>
-          <td>{sortedData[3].first_name + " " + sortedData[3].last_name}</td>
-          <td>{sortedData[3].total_rx.reduce((a, b) => a + b, 0)}</td>
-        </tr>
-        <tr>
-          <td>{sortedData[4].first_name + " " + sortedData[4].last_name}</td>
-          <td>{sortedData[4].total_rx.reduce((a, b) => a + b, 0)}</td>
-        </tr>
+        {tabledata()}
       </table>
     </div>
   )
